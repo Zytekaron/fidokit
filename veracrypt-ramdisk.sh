847bddc907df3e1180d10d817c1d929666c494a7
Michael Thornes <michael@zyte.dev>
2025-07-13 05:33:57 -0700

    fix veracrypt ramdisk script script tested and working on macOS

--- veracrypt-ramdisk.sh
+++ veracrypt-ramdisk.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 set -euo pipefail
 
-VC_VOLUME=$1
+VC_VOLUME="$1"
 
 OS=$(uname)
 
 if [ "$OS" = "Linux" ]; then
     # Linux: Use /dev/shm
-    KEYFILE="/dev/shm/keyfile_$$.bin"
+    RAND=$(($(od -An -N2 -tu2 /dev/urandom | tr -d ' ') % 900000 + 100000))
+    KEYFILE="/dev/shm/keyfile_$RAND.bin"
 
-    ./fidoutil -o "$KEYFILE" "$@"
-    ./veracrypt --keyfiles "$KEYFILE" "$VC_VOLUME"
+    echo "Starting fidokit in unlock mode:"
+    ./fidokit -U -o "$KEYFILE" "$@" || true
 
-    srm "$KEYFILE" || shred -u "$KEYFILE" || rm "$KEYFILE"
+    veracrypt -t --keyfiles "$KEYFILE" "$VC_VOLUME" || true
+
+    srm "$KEYFILE" 2>/dev/null || shred -u "$KEYFILE" 2>/dev/null || rm "$KEYFILE"
 
 elif [ "$OS" = "Darwin" ]; then
     # macOS: Create RAM disk
     DISK=$(hdiutil attach -nomount ram://2048) # 512 bytes × 2048 = 1 MB
     RAMDISK_NAME="ramdisk_$(jot -r 1 100000 999999)"
-    diskutil erasevolume HFS+ "$RAMDISK_NAME" "$DISK" > /dev/null
-    MNT="/Volumes/$RAMDISK_NAME"
-    KEYFILE="$MNT/keyfile.bin"
+    diskutil erasevolume HFS+ "$RAMDISK_NAME" $DISK > /dev/null # NOTE: DO NOT QUOTE $DISK
+    MOUNTPOINT="/Volumes/$RAMDISK_NAME"
+    KEYFILE="$MOUNTPOINT/keyfile.bin"
 
-    ./fidoutil -o "$KEYFILE" "$@"
-    ./veracrypt --keyfiles "$KEYFILE" "$VC_VOLUME"
+    echo "Starting fidokit in unlock mode:"
+    ./fidokit -U -o "$KEYFILE" "$@" || true
 
-    srm "$KEYFILE" || shred -u "$KEYFILE" || rm "$KEYFILE"
-    diskutil unmount "$MNT"
-    hdiutil detach "$DISK"
+    veracrypt -t --keyfiles "$KEYFILE" "$VC_VOLUME" || true
+
+    srm "$KEYFILE" 2>/dev/null || shred -u "$KEYFILE" 2>/dev/null || rm "$KEYFILE"
+    diskutil unmount "$MOUNTPOINT"
+    hdiutil detach $DISK # NOTE: DO NOT QUOTE $DISK
 
 else
     echo "Unsupported OS: $OS" >&2
